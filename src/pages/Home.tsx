34f05413379f0ec7cbbc3381ef5f7f07790ee74b
Ahmed Zeno <ahmad.zeino@email.com>
2026-04-11 16:50:48 +0200

    chore: enable strict mode and remove dead legacy files

--- src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h2>Welcome to the Holocron Archives</h2>
-      <p>Select a category above to begin exploring.</p>
-    </div>
-  )
-}
